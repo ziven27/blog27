9f4ccfd94c5e52cd1f3f17e8e546387a591d5994
ziven <zhengcz@knownsec.com>
2015-10-09 09:48:57 +0000

    up

---
+++ static/js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+	var hm = document.createElement("script");
+	hm.src = "//hm.baidu.com/hm.js?c2751207a7ac225c405e77ec498f1273";
+	var s = document.getElementsByTagName("script")[0];
+	s.parentNode.insertBefore(hm, s);
+})();
